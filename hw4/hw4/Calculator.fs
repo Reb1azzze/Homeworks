a30b4ddfdb39f08f9dc7094def1b7dfdb5cc920f
Iskander <faceitlekro@yandex.ru>
2021-10-11 17:42:39 +0300

    Homework F# is trying to be commited

---
+++ hw4/hw4/Calculator.fs
@@ -0,0 +1,10 @@
+﻿module hw4.Calculator
+
+let calculate (val1: int) operation (val2:int) =
+        match operation with
+        |"+" -> val1 + val2
+        |"-" -> val1 - val2
+        |"*" -> val1 * val2
+        |"/" -> val1 / val2
+        | _ -> 0
+       
---
+++ hw4/hw4/Program.fs
@@ -0,0 +1,15 @@
+﻿module hw4.Program
+
+[<EntryPoint>]
+let main args =
+    let mutable val1 = 0
+    let mutable val2 = 0
+    let mutable operation = ""
+    
+    let parseResult = Parser.ParseArgs args &val1 &operation &val2
+
+    if parseResult <> 0 then parseResult
+    else
+        let result = Calculator.calculate val1 operation val2
+        printf $"{val1}{operation}{val2}={result}"
+        0
