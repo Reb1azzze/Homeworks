c8aa6dee444328e33351f178db65b8ba9bd8f772
Iskander <faceitlekro@yandex.ru>
2021-11-27 13:51:00 +0300

    hw8 is done

---
+++ hw8/hw8/Logic/NumericalCalculator.cs
@@ -0,0 +1,19 @@
+﻿using hw8.Interface;
+
+namespace hw8
+{
+	public class NumericalCalculator : ICalculator
+	{
+		public double Add(double fVal, double sVal)
+			=> fVal + sVal;
+
+		public double Subtract(double fVal, double sVal)
+			=> fVal - sVal;
+
+		public double Divide(double fVal, double sVal)
+			=> sVal == 0 ? 0 : fVal / sVal;
+
+		public double Multiply(double fVal, double sVal)
+			=> fVal * sVal;
+	}
+}
