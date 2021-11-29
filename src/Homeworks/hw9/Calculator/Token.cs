5491536b9efeae912361281e9a9d03e0516f1a33
Iskander <faceitlekro@yandex.ru>
2021-11-29 18:04:32 +0300

    hw9 is done

---
+++ src/Homeworks/hw9/Calculator/Token.cs
@@ -0,0 +1,21 @@
+﻿namespace hw9.Calculator
+{
+    public enum TokenType
+    {
+        Number,
+        Operation,
+        Bracket
+    }
+
+    public struct Token
+    {
+        public readonly TokenType Type;
+        public readonly string Value;
+
+        public Token(TokenType type, string value)
+        {
+            Type = type;
+            Value = value;
+        }
+    }
+}
---
+++ src/Homeworks/hw9/Controllers/CalculatorController.cs
@@ -0,0 +1,34 @@
+﻿using hw9.Calculator;
+using hw9.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HW9.Controllers
+{
+    public class CalculatorController : Controller
+    {
+        private readonly ICalculator _calculator;
+
+        public CalculatorController(ICalculator calculator)
+        {
+            _calculator = calculator;
+        }
+
+        [HttpGet]
+        public IActionResult Calculate()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Calculate(string expression)
+        {
+            var result = _calculator.Calculate(expression);
+            AnswerModel model;
+            if (result.Type is Answer.Success)
+                model = new AnswerModel($"Result: {result.Success}");
+            else
+                model = new AnswerModel($"Error: {result.Error}");
+            return View(model);
+        }
+    }
+}
