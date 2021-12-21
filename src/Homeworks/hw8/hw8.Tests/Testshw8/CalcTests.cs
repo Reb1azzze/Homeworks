bb7bd04ae4aed8fc57c15dc891b4e60af32869b0
Iskander <faceitlekro@yandex.ru>
2021-12-21 03:12:48 +0300

    hw8 fix

--- src/Homeworks/hw8/hw8.Tests/Testshw8/CalcTests.cs
+++ src/Homeworks/hw8/hw8.Tests/Testshw8/CalcTests.cs
@@ -1,64 +1,71 @@
-using hw8;
-using System;
 using Xunit;
 
-namespace Testshw8
+namespace hw8.Tests
 {
-	public class CalcTests
-	{
-		private static readonly NumericalCalculator calculator = new();
+    public class CalcTests
+    {
+        [Theory]
+        [InlineData(1, 2, "3")]
+        [InlineData(-1, -1, "-2")]
+        [InlineData(5.4, 1.3, "6.7")]
+        public void Plus_TwoNumbers_ReturnSum(double val1, double val2, string expResult)
+        {
+            //arrange
+            Calculator.Calculator calculator = new Calculator.Calculator();
 
-		[Theory]
-		[InlineData(1, 2, 3)]
-		[InlineData(-2, 3, 1)]
-		[InlineData(2, -4, -2)]
-		[InlineData(-3, -4, -7)]
-		public void Add_AddTwoNumbers_ReturnSum(
-			int firstNum, int secondNum, int expected)
-		{
-			var actual = calculator.Add(firstNum, secondNum);
-			Assert.Equal(expected, actual);
-		}
+            //act
+            var actual = calculator.Plus(val1, val2);
 
-		[Theory]
-		[InlineData(5, 2, 3)]
-		[InlineData(2, -6, 8)]
-		[InlineData(-3, 4, -7)]
-		[InlineData(-8, -2, -6)]
-		public void Subtract_ReturnDifference(
-			int firstNum, int secondNum, int expected)
-		{
-			var actual = calculator.Subtract(firstNum, secondNum);
-			Assert.Equal(expected, actual);
-		}
+            //assert
+            Assert.Equal(actual, expResult);
+        }
 
-		[Theory]
-		[InlineData(0, 0, 0)]
-		[InlineData(0, -6, 0)]
-		[InlineData(4, 0, 0)]
-		[InlineData(1, 20, 20)]
-		[InlineData(3, -2, -6)]
-		[InlineData(-5, -3, 15)]
-		public void Multiply_ReturnProduct(
-			int firstNum, int secondNum, int expected)
-		{
-			var actual = calculator.Multiply(firstNum, secondNum);
-			Assert.Equal(expected, actual);
-		}
+        [Theory]
+        [InlineData(1, 2, "-1")]
+        [InlineData(-5.5, 2, "-7.5")]
+        [InlineData(10, -24.3, "34.3")]
+        public void Minus_TwoNumbers_ReturnDiff(double val1, double val2, string expResult)
+        {
+            //arrange
+            Calculator.Calculator calculator = new Calculator.Calculator();
 
-		[Theory]
-		[InlineData(20, 2, 10)]
-		[InlineData(10, -5, -2)]
-		[InlineData(-9, -9, 1)]
-		[InlineData(0, 2, 0)]
-		[InlineData(0, -10, 0)]
-		[InlineData(12, 0, 0)]
-		[InlineData(0, 0, 0)]
-		public void Divide_ReturnQuotient(
-			int firstNum, int secondNum, int expected)
-		{
-			var actual = calculator.Divide(firstNum, secondNum);
-			Assert.Equal(expected, actual);
-		}
-	}
+            //act
+            var actual = calculator.Minus(val1, val2);
+
+            //assert
+            Assert.Equal(actual, expResult);
+        }
+
+        [Theory]
+        [InlineData(1, 2, "2")]
+        [InlineData(-5.5, 2, "-11")]
+        [InlineData(0, 24.3, "0")]
+        public void Multiply_TwoNumbers_ReturnMultiplication(double val1, double val2, string expResult)
+        {
+            //arrange
+            Calculator.Calculator calculator = new Calculator.Calculator();
+
+            //act
+            var actual = calculator.Multiply(val1, val2);
+
+            //assert
+            Assert.Equal(actual, expResult);
+        }
+
+        [Theory]
+        [InlineData(1, 2, "0.5")]
+        [InlineData(-5, 2, "-2.5")]
+        [InlineData(110, 0, "divide by zero")]
+        public void Divide_TwoNumbers_ReturnQuotient(double val1, double val2, string expResult)
+        {
+            //arrange
+            Calculator.Calculator calculator = new Calculator.Calculator();
+
+            //act
+            var actual = calculator.Divide(val1, val2);
+
+            //assert
+            Assert.Equal(actual, expResult);
+        }
+    }
 }
--- src/Homeworks/hw8/hw8.Tests/Testshw8/ProgramTests.cs
+++ src/Homeworks/hw8/hw8.Tests/Testshw8/ProgramTests.cs
@@ -5,73 +5,54 @@ using Microsoft.AspNetCore.Mvc.Testing;
 using Microsoft.AspNetCore.TestHost;
 using Microsoft.Extensions.Hosting;
 using Xunit;
-using hw8;
 
-namespace Testshw8
+namespace hw8.Tests
 {
-	public class HostBuilder : WebApplicationFactory<Startup>
-	{
-		protected override IHostBuilder CreateHostBuilder()
-			=> Host
-				.CreateDefaultBuilder()
-				.ConfigureWebHostDefaults(a => a
-					.UseStartup<Startup>()
-					.UseTestServer());
-	}
+    public class HostBuilder : WebApplicationFactory<Startup>
+    {
+        protected override IHostBuilder CreateHostBuilder() =>
+            Host
+                .CreateDefaultBuilder()
+                .ConfigureWebHostDefaults(a => a
+                    .UseStartup<Startup>()
+                    .UseTestServer());
+    }
 
-	public class ProgramTests : IClassFixture<HostBuilder>
-	{
-		private readonly HttpClient client;
-		public ProgramTests(HostBuilder fixture)
-		{
-			client = fixture.CreateClient();
-		}
+    public class ProgramTests : IClassFixture<HostBuilder>
+    {
+        private const string Path = "https://localhost:5001/Calculator/Calculate";
+        private readonly HttpClient _client;
 
-		[Theory]
-		[InlineData("2", "Add", "4", "6")]
-		[InlineData("10", "Subtract", "4", "6")]
-		[InlineData("0", "Multiply", "5", "0")]
-		[InlineData("10", "Divide", "5", "2")]
-		[InlineData("2.3", "Add", "3.1", "5.4")]
-		[InlineData("2.5", "Multiply", "0", "0")]
-		[InlineData("1.5", "Divide", "1.5", "1")]
-		public async Task Main_CorrectExpression_ReturnCorrectResult(string v1,
-			string operation,
-			string v2,
-			string expected)
-		{
-			await DoTest(v1, operation, v2, expected);
-		}
+        public ProgramTests(HostBuilder fixture)
+        {
+            _client = fixture.CreateClient();
+        }
 
-		[Theory]
-		[InlineData("x", "Add", "4", "4")]
-		[InlineData("p", "Subtract", "4", "-4")]
-		[InlineData("dream", "Multiply", "team", "0")]
-		public async Task Main_InvalidExpression_ReturnMessageError(string v1,
-			string operation,
-			string v2,
-			string expected)
-		{
-			await DoTest(v1, operation, v2, expected);
-		}
+        [Theory]
+        [InlineData("2", "plus", "59", "61")]
+        [InlineData("11", "minus", "17", "-6")]
+        [InlineData("3", "divide", "1", "3")]
+        [InlineData("2021", "multiply", "4", "8084")]
+        public async Task Calculate_ValidArguments_CorrectResult(string v1, string op, string v2, string excepted)
+        {
+            await MakeGeneralPartTests(v1, op, v2, excepted);
+        }
 
-		[Theory]
-		[InlineData("10", "Divide", "0", "0")]
-		[InlineData("2.4", "Divide", "0", "0")]
-		public async Task Main_DivideZero_ReturnMessageError(string v1,
-			string operation,
-			string v2,
-			string expected)
-		{
-			await DoTest(v1, operation, v2, expected);
-		}
+        [Theory]
+        [InlineData("invalid", "plus", "5", "wrong args")]
+        [InlineData("5", "minus", "error", "wrong args")]
+        [InlineData("5", "power", "5", "plus, minus, multiply или divide")]
+        [InlineData("301", "divide", "0", "divide by zero")]
+        public async Task Calculate_InvalidArguments_ErrorMessage(string v1, string op, string v2, string excepted)
+        {
+            await MakeGeneralPartTests(v1, op, v2, excepted);
+        }
 
-		private async Task DoTest(string v1, string operation, string v2, string expected)
-		{
-			var response =
-				await client.GetAsync($"https://localhost:5001/Calculator/{operation}?val1={v1}&val2={v2}");
-			var result = await response.Content.ReadAsStringAsync();
-			Assert.Equal(expected, result);
-		}
-	}
+        private async Task MakeGeneralPartTests(string v1, string op, string v2, string excepted)
+        {
+            var response = await _client.GetAsync($"{Path}?val1={v1}&operation={op}&val2={v2}");
+            var result = await response.Content.ReadAsStringAsync();
+            Assert.Equal(excepted, result);
+        }
+    }
 }
